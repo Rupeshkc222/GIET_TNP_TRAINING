14da48ce7ea85d136cd173e70513a433ae8fc328
RUPESH KUMAR CHATRI <78075295+Rupeshkc222@users.noreply.github.com>
2023-04-05 23:11:05 +0530

    Delete GIET_Training-main/GIET_TNP_TRAINING directory

--- GIET_Training-main/GIET_TNP_TRAINING/Day2(functions)/translate using dictionary.py
+++
@@ -1,7 +0,0 @@
-n=["merry","and"]
-d={"merry":"god","christmas":"jul","and":"och","happy":"gott","new":"nytt","year":"ar"}
-res=[]
-for i in n:
-    res.append(d[i])
-print(res)
-                                   
